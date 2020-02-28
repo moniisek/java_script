310e96bf0214c423285f7a357b7f4a5c6f37c233
helena <helenapaulasova@hotmail.com>
2020-02-28 15:33:12 +0100

    cleaning

--- React/Authentication/src/context/auth.js
+++
@@ -1,9 +0,0 @@
-import {createContext, useContext} from 'react'
-
-export const AuthContext = createContext();
-
-
-// useAuth is a custom hook
-export function useAuth() {
-  return useContext(AuthContext);
-}
